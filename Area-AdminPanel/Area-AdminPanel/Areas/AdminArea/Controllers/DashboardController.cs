4f94e4a9e3636357d85638049948393a6297e0d5
fidanmardanli <fidannm@code.edu.az>
2022-10-11 00:24:01 +0400

    task

---
+++ Area-AdminPanel/Area-AdminPanel/Areas/AdminArea/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Area_AdminPanel.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class DashboardController : Controller
+    {
+ 
+        public  IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
